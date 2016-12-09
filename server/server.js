a86c3231f23d44c6a914efd57dc28bcd346ef9d8
stan <dhmoon91@gmail.com>
2016-12-09 14:18:40 -0500

    Setup app for heroku

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} =require('mongodb');
 
 //local imports
 var {mongoose} = require('./db/mongoose.js');
@@ -8,6 +9,7 @@ var {User} = require('./models/user');
 
 var app = express();
 
+const port = process.env.PORT || 3000;
 //can send json to our express app/
 app.use(bodyParser.json());
 
@@ -33,8 +35,33 @@ app.get('/todos', (req,res) => {
   });
 });
 
-app.listen ( 3000, () => {
-  console.log("On port 3000");
+//GET /todos/12312 <- dynamic
+app.get('/todos/:id', (req,res) =>{
+  //object, key value pair. key= :id
+  var id = req.params.id;
+
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then( (todo) => {
+    if (!todo) {
+      return res.satus(404).send();
+    }
+    res.send( {todo});
+  }).catch((e) => {res.status(400).send();
+  });
+  //validate id using isValid
+  //404 -send back emtpy send
+
+  //query database; findById
+
+
+  //res.send(req.params);
+});
+
+app.listen ( port, () => {
+  console.log(`Started up at port ${port}`);
 });
 //working mongoose model.
 // var Todo = mongoose.model('Todo', {
